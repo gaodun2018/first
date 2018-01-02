d9ff31a40a025af208d13c1b3c8474e6b7de63ba
zhangxin <zhangxin@gaodun.com>
2018-01-02 15:17:37 +0800

    课程列表搜索 saas/document#19

--- src/js/components/resourceCenter/AddVideo.vue
+++ src/js/components/resourceCenter/AddVideo.vue
@@ -18,17 +18,20 @@
           </el-select>
         </el-form-item>
         <el-form-item label="视频名称" prop="name">
-          <el-input v-model="ruleForm.name"  auto-complete="off" class="w_50"></el-input>
+          <el-input v-model="ruleForm.name" auto-complete="off" class="w_50"></el-input>
         </el-form-item>
         <el-form-item label="备注说明" prop="name">
-          <el-input v-model="ruleForm.name"  auto-complete="off" class="w_50"></el-input>
+          <el-input v-model="ruleForm.name" auto-complete="off" class="w_50"></el-input>
         </el-form-item>
         <el-form-item label="视频地址" prop="name">
-          <el-input v-model="ruleForm.name"  auto-complete="off" class="w_60"></el-input>
+          <el-input v-model="ruleForm.name" auto-complete="off" class="w_60"></el-input>
           <el-button type="text" @click="" style="margin-left: 20px;">本地上传</el-button>
         </el-form-item>
         <el-form-item label="视频时长" prop="name" class="displayinline">
-          <el-input v-model="ruleForm.name" auto-complete="off"></el-input>分<el-input v-model="ruleForm.name" auto-complete="off" style="margin-left: 20px;"></el-input>秒
+          <el-input v-model="ruleForm.name" auto-complete="off"></el-input>
+          分
+          <el-input v-model="ruleForm.name" auto-complete="off" style="margin-left: 20px;"></el-input>
+          秒
         </el-form-item>
         <el-form-item label="知识点关联" prop="name">
           <el-button type="text" @click="">选择知识点</el-button>
@@ -41,31 +44,38 @@
     </div>
 
     <el-dialog class="selectknowledge" title="请选择知识点关联" :visible.sync="dialogVisible" @close="closeDialog('ruleForm')">
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-row class="searchtools">
+        <el-col :span="8">
+          <span>一级分类：</span>
+          <el-select v-model="groupVal" placeholder="请选择所属项目" class="group-search" @change="changesearch"
+                     @visible-change="visibleChange">
+            <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="8">
+          <span>二级分类：</span>
+          <el-select v-model="completeVal" placeholder="请选择所属科目" class="complete-search" @change="changesearch"
+                     @visible-change="visibleChange">
+            <el-option v-for="item in efficiency" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="8">
+          <el-input placeholder="输入学员ID/手机号/昵称/学员姓名" icon="search" class="inputsearch" v-model="inuptVal"
+                    :on-icon-click="handleIconClick" @keydown.native.enter="handleIconClick">
+          </el-input>
+        </el-col>
+      </el-row>
 
-        <el-form-item label="一级分类" prop="project_id">
-          <el-select v-model="ruleForm.project_id" placeholder="请选择所属项目" @change="changeProject">
-            <el-option :label="item.project_name" :value="item.project_id" v-for="(item,index) in projectlist" :key="item.project_id"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="所属科目" prop="subject_id">
-          <el-select v-model="ruleForm.subject_id" :disabled="!bSubject" placeholder="请选择所属科目">
-            <el-option :label="item.subject_name" :value="item.subject_id" v-for="(item,index) in selectedlist" :key="item.subject_id"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="网课类型" prop="course_type_id">
-          <el-select v-model="ruleForm.course_type_id" placeholder="请选择网课类型">
-            <el-option :label="item.name" :value="item.course_type_id" v-for="item in course_type" :key="item.course_type_id"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <el-table ref="multipleTable" :data="tableData3" border tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table ref="multipleTable" :data="tableData3" border tooltip-effect="dark" style="width: 100%"
+                @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55">
         </el-table-column>
-        <el-table-column label="日期" width="120">
+        <el-table-column label="日期" width="30%">
           <template scope="scope">{{ scope.row.date }}</template>
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
+        <el-table-column prop="name" label="姓名" width="30%">
         </el-table-column>
         <el-table-column prop="address" label="地址" show-overflow-tooltip>
         </el-table-column>
@@ -85,7 +95,7 @@
     components: {},
     data() {
       return {
-        dialogVisible:true,
+        dialogVisible: true,
         ruleForm: {
           name: '',
           region: '',
@@ -98,42 +108,42 @@
         },
         rules: {
           name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+            {required: true, message: '请输入活动名称', trigger: 'blur'},
+            {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
           ],
           region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
+            {required: true, message: '请选择活动区域', trigger: 'change'}
           ],
           date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+            {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
           ],
           date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+            {type: 'date', required: true, message: '请选择时间', trigger: 'change'}
           ],
           type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+            {type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change'}
           ],
           resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
+            {required: true, message: '请选择活动资源', trigger: 'change'}
           ],
           desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
+            {required: true, message: '请填写活动形式', trigger: 'blur'}
           ]
         },
         tableData3: [
           {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }
         ],
         multipleSelection: []
       }
