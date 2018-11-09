41a0bda7db4895a624010572c4dc46afc7a61ebb
xiongzuyan <xiongzuyan@gaodun.com>
2018-11-09 11:24:06 +0800

    fix

--- src/components/SyllabusManage/SyllabusGliveEdit.vue
+++ src/components/SyllabusManage/SyllabusGliveEdit.vue
@@ -126,14 +126,14 @@
           <el-form-item label="显示名称" prop="name" :rules="filter_rules({required:true,type:'isAllSpace',maxLength:100})">
             <el-input class="coursetxt" v-model="addResFirFrom.name" @keydown.native.enter="firstNextSubmit('addResFirFrom')"></el-input>
           </el-form-item>
-          <el-form-item label="学习环节" prop="type" :rules="[ { required: true, trigger: 'change', message: '请选择学习环节！' }]">
+          <el-form-item label="学习环节" prop="type" :rules="filter_rules({required:true,type:'isAllSpace'})">
             <el-radio-group v-model="addResFirFrom.type">
               <el-radio :label="1">课前</el-radio>
               <el-radio :label="2" v-if='hasType2'>课中</el-radio>
               <el-radio :label="3">课后</el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="课程类型" prop="required" :rules="[ { required: true, trigger: 'change', message: '请选择课程类型！' }]">
+          <el-form-item label="课程类型" prop="required" :rules="filter_rules({required:true,type:'isAllSpace'})">
             <el-radio-group v-model="addResFirFrom.required">
               <el-radio :label="1">必修</el-radio>
               <el-radio :label="0">选修</el-radio>
@@ -152,7 +152,7 @@
               {{item.label}}
             </span>
           </div>
-          <el-form-item label="开启时间" v-if='addResFirFrom.type==2' :rules="[ { required: true,message: '请设置开启时间', trigger: 'blur' }]" style='margin-top: 80px;'>
+          <el-form-item label="开启时间" v-if='addResFirFrom.type==2' :rules="filter_rules({required:true,type:'isAllSpace'})" style='margin-top: 80px;'>
             <el-date-picker v-model="addResFirFrom.start_time" type="datetime" value-format="timestamp" placeholder="请设置开启时间" format='yyyy-MM-dd HH:mm' @change='checkTime(addResFirFrom.start_time)'>
             </el-date-picker>
           </el-form-item>
